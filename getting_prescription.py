7716d626cf91012cf11a942ff49ecba8e5519675
Nolexnol <nobleagyemanbobie2023@gmail.com>
2024-11-19 10:42:16 -0600

    COmmit

--- getting_prescription.py
+++ getting_prescription.py
@@ -5,18 +5,20 @@ with open("eye_patient_info.csv", "r") as file:
     diagnosis = {}
     # Diagnosis will contain drug, dosage, frequency
     for datarow in patients_data:
-        diagnosis["diagnosis"] = {"diagnosis": datarow["diagnosis"], "icd_code": datarow["icd_code"],"Drug": datarow["drugname"], "Frequency": datarow["Frequency"]}
+        diagnosis["diagnosis"] = {"diagnosis": datarow["diagnosis"], "icd_code": datarow["icd_code"], \
+                                  "Drug": datarow["drugname"], "Frequency": datarow["Frequency"]}
         diagnosis["dosage"] = datarow["dosage"]
 file.close()
 
-with open("drugfile.csv","r") as drugfile:
-    drugfile = csv.DictReader(drugfile)
-    for druginfo in drugfile:
-        if diagnosis["drug"] in druginfo:
-            if druginfo["controlled"] == "Y":
-                #replace with a non-controlled drug
-                #add the price to the pharmacy data
-        else:
-            #replace with an available drug
-            #add the price of the drug to the pharmacy data
-drugfile.close()
+with open("c_cs_alpha (1).csv","r") as drugs_data_file:
+    drugs_data_file = csv.DictReader(drugs_data_file)
+    for drug in drugs_data_file:
+        if diagnosis["drugname"] == drug["Substance"] or diagnosis["drugname"] == drug["Other_Name"]:
+            if drug["Controlled"] == "Y":
+                continue
+            else:
+                diagnosis["drugname"] = drug["Substance"]
+                
+                
+                
+                
